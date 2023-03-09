1778e7f9a15fed955f3ee36620f7f370499e9ad5
johnta0 <22534726+johnta0@users.noreply.github.com>
2023-03-09 23:16:54 +0900

    Move theme

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,6 +1,6 @@
 import { extendTheme } from '@chakra-ui/react'
 
-export const theme = extendTheme({
+const theme = extendTheme({
   styles: {
     global: {
       body: {
@@ -9,3 +9,5 @@ export const theme = extendTheme({
     },
   },
 })
+
+export default theme
