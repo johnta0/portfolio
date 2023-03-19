b32b98fe40f90f5861719f0c2dc58355db1418c1
johnta0 <22534726+johnta0@users.noreply.github.com>
2023-03-19 12:05:22 +0900

    Decorate by underline for section-title variant

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,12 +1,29 @@
-import { extendTheme } from '@chakra-ui/react'
+import { defineStyleConfig, extendTheme } from '@chakra-ui/react'
+
+const Heading = defineStyleConfig({
+  variants: {
+    'section-title': {
+      textDecoration: 'underline',
+      fontSize: 20,
+      textUnderlineOffset: 6,
+      textDecorationColor: '#525252',
+      textDecorationThickness: 4,
+      marginTop: 3,
+      marginBottom: 4,
+    },
+  },
+})
 
 const theme = extendTheme({
   styles: {
     global: {
       body: {
-        backgroundColor: 'black',
+        backgroundColor: 'gray.900',
       },
     },
+  },
+  components: {
+    Heading,
   },
 })
 
